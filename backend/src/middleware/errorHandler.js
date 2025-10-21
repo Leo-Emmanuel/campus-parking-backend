9dc49b59ffac6a3773c9c556d03696cbbdb92e58
Leo-Emmanuel <leoemmanuel2004@gmail.com>
2025-10-21 15:39:34 +0530

    chore: Remove duplicate src folder

--- backend/src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
- module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-
-  res.status(err.statusCode || 500).json({
-    status: 'error',
-    message: err.message || 'Server Error',
-  });
-};
